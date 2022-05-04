97f3314a838de4380558be74991ab1c3d889ee8f
elisa.preynat <elisa.preynat@isen.yncrea.fr>
2022-05-04 14:51:44 +0200

    Arrangement d'un problème de toolbar qui marchait pas sur une activité

--- app/src/main/java/fr/logkey/logkeyapp/moncompte/ModificationMDP.kt
+++ app/src/main/java/fr/logkey/logkeyapp/moncompte/ModificationMDP.kt
@@ -8,9 +8,15 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import fr.logkey.logkeyapp.MenuHambActivity
 import fr.logkey.logkeyapp.R
+import fr.logkey.logkeyapp.accueil.AccueilActivity
+import fr.logkey.logkeyapp.accueil.MaReservationActivity
+import fr.logkey.logkeyapp.accueil.MonCompteActivity
+import fr.logkey.logkeyapp.accueil.NotificationsActivity
 import fr.logkey.logkeyapp.ui.login.EmailPasswordActivity
 
 class ModificationMDP : AppCompatActivity() {
@@ -23,6 +29,11 @@ class ModificationMDP : AppCompatActivity() {
 
     //Les références Firebase
     private var auth: FirebaseAuth? = null
+    lateinit var bellButton : ImageView
+    lateinit var bagButton : ImageView
+    lateinit var homeButton : ImageView
+    lateinit var userButton : ImageView
+    lateinit var menuButton : ImageView
 
     private fun initialisation() {
         eMailAddress = findViewById<View>(R.id.editTexteEmailChangementMDP) as EditText
@@ -61,6 +72,39 @@ class ModificationMDP : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modification_mdp)
+        bellButton = findViewById(R.id.bell)
+        bagButton = findViewById(R.id.bag)
+        homeButton = findViewById(R.id.house)
+        userButton = findViewById(R.id.user)
+        menuButton = findViewById(R.id.menu)
+
+        val intent1 = Intent(this, NotificationsActivity::class.java)
+        bellButton.setOnClickListener {
+            startActivity(intent1)
+        }
+
+
+        val intent2 = Intent(this, MaReservationActivity::class.java)
+        bagButton.setOnClickListener {
+            startActivity(intent2)
+        }
+
+        val intent3 = Intent(this, AccueilActivity::class.java)
+        homeButton.setOnClickListener {
+            startActivity(intent3)
+        }
+
+
+        val intent4 = Intent(this, MonCompteActivity::class.java)
+        userButton.setOnClickListener {
+            startActivity(intent4)
+        }
+
+        val intent5 = Intent(this, MenuHambActivity::class.java)
+        menuButton.setOnClickListener {
+            startActivity(intent5)
+        }
+
         initialisation()
     }
 }
