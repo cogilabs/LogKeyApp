55df07acb897cbb3092a976c4c883ad649ac3240
elisa.preynat <elisa.preynat@isen.yncrea.fr>
2022-05-05 11:47:01 +0200

    Récupération de données Firestore Etape 1

---
+++ app/src/main/java/fr/logkey/logkeyapp/database/UpdateData.kt
@@ -0,0 +1,10 @@
+package fr.logkey.logkeyapp.database
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/fr/logkey/logkeyapp/moncompte/InformationsPersonnellesActivity.kt
+++ app/src/main/java/fr/logkey/logkeyapp/moncompte/InformationsPersonnellesActivity.kt
@@ -1,9 +1,16 @@
 package fr.logkey.logkeyapp.moncompte
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
 import fr.logkey.logkeyapp.MenuHambActivity
 import fr.logkey.logkeyapp.R
 import fr.logkey.logkeyapp.accueil.AccueilActivity
@@ -13,19 +20,65 @@ import fr.logkey.logkeyapp.accueil.NotificationsActivity
 
 
 class InformationsPersonnellesActivity : AppCompatActivity() {
+    //Variables base de données
+    lateinit var userName : TextView
+    lateinit var userSurname : TextView
+    lateinit var userBirthDate : TextView
+    lateinit var userEmail : TextView
+    lateinit var userPhone : TextView
+    lateinit var userPostalAddress : TextView
+
+    //Variables ToolBar
     lateinit var bellButton : ImageView
     lateinit var bagButton : ImageView
     lateinit var homeButton : ImageView
     lateinit var userButton : ImageView
     lateinit var menuButton : ImageView
+
+    //Initialisation variable pour firebase firestore
+    var db: FirebaseFirestore? = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_informations_personnelles)
+
+        //DataBase
+        userName = findViewById(R.id.nom)
+
+
+        //Toolbar
         bellButton = findViewById(R.id.bell)
         bagButton = findViewById(R.id.bag)
         homeButton = findViewById(R.id.house)
         userButton = findViewById(R.id.user)
         menuButton = findViewById(R.id.menu)
+
+        //Création de variable pour la référence du doc dans la bdd
+        val documentReference = db!!.collection("Client").document("1GJn4a4efqD8pztWQhoQ")
+
+        // adding snapshot listener to our document reference.
+        // adding snapshot listener to our document reference.
+        documentReference.addSnapshotListener(object : EventListener<DocumentSnapshot?> {
+            override fun onEvent(
+                @Nullable value: DocumentSnapshot?,
+                @Nullable error: FirebaseFirestoreException?
+            ) {
+                // inside the on event method.
+                if (error != null) {
+                    // this method is called when error is not null
+                    // and we gt any error
+                    // in this cas we are displaying an error message.
+                    Toast.makeText(this@InformationsPersonnellesActivity, "Error found is $error", Toast.LENGTH_SHORT)
+                        .show()
+                    return
+                }
+                if (value != null && value.exists()) {
+                    // if th value from firestore is not null then we are getting
+                    // our data and setting that data to our text view.
+                    userName.text = value.data!!["surname"].toString()
+                }
+            }
+        })
 
         val intent1 = Intent(this, NotificationsActivity::class.java)
         bellButton.setOnClickListener {
